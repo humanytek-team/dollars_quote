362a047d76c85fadeee71d477c836fd51dd80ba3
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2018-05-25 09:27:08 -0500

    [IMP] usd_com, workforce and gif fields

--- models/product_product.py
+++ models/product_product.py
@@ -3,3 +3,24 @@ from odoo import api, fields, models
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
+
+    usd_com = fields.Monetary(
+        currency_field='usd_currency_id',
+        string='USD Cost of Materials',
+    )
+    workforce = fields.Monetary(
+        currency_field='usd_currency_id',
+    )
+    gif = fields.Monetary(
+        currency_field='usd_currency_id',
+        string='GIF',
+    )
+    usd_currency_id = fields.Many2one(
+        comodel_name='res.currency',
+        compute='_get_usd_currency_id',
+    )
+
+    @api.depends('usd_com')
+    @api.one
+    def _get_usd_currency_id(self):
+        self.usd_currency_id = self.env.ref('base.USD')
