f309d0f1edbec513caa9a0ab568ec9eff5d1767f
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2018-05-25 10:35:55 -0500

    [REF]

--- models/product_product.py
+++ models/product_product.py
@@ -20,7 +20,6 @@ class ProductProduct(models.Model):
         compute='_get_usd_currency_id',
     )
 
-    @api.depends('usd_com')
     @api.one
     def _get_usd_currency_id(self):
         self.usd_currency_id = self.env.ref('base.USD')
--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -12,7 +12,6 @@ class SaleOrderLine(models.Model):
         compute='_get_usd_currency_id',
     )
 
-    @api.depends('usd_com')
     @api.one
     def _get_usd_currency_id(self):
         self.usd_currency_id = self.env.ref('base.USD')
